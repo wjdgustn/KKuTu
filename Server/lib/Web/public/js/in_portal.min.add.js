9de623a309f67ba9f416c7280011dc91cc9c920a
wjdgustn <34962071+wjdgustn@users.noreply.github.com>
2020-02-14 20:07:43 +0900

    Add files via upload

---
+++ Server/lib/Web/public/js/in_portal.min.add.js
@@ -0,0 +1,18 @@
+// 여기부터 해시 처리
+if (location.hash=='#sans') {
+    alert("와!")
+	alert("샌즈 아시는구나!")
+    location.href='https://kkutu.org/sans/index.htm'
+}
+
+if (location.hash=='#mario') {
+	alert("마리오!")
+	alert("이예에에에에에!")
+	location.href='https://kkutu.org/mario/index.htm'
+}
+
+if (location.hash=='#spinner') {
+	alert("빙글빙글")
+	alert("빙글빙글빙글빙글빙글빙글빙글빙글빙글빙글빙글빙글빙글빙글빙글빙글")
+	location.href='https://kkutu.org/spinner/index.htm'
+}
---
+++ Server/lib/Web/public/js/nogmkicker.js
@@ -0,0 +1,4 @@
+if (location.search=='?server=1') {
+	alert("권한이 없습니다.")
+	location.href="https://kkutu.org"
+}
